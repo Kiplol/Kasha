48cea3ba399226bb3975ac7c5d7311173877aa1e
Elliott "Kip" Kipper <Kip@Phyllis.local>
2018-06-04 21:51:14 -0700

    Project

---
+++ Kasha/Kasha/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Kasha
+//
+//  Created by Elliott Kipper on 6/4/18.
+//  Copyright © 2018 Kip. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
