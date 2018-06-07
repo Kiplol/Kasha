2e3860848f4181a4d5bd9bfe0700ce79e5ef1cd4
Elliott "Kip" Kipper <Kip@Phyllis.local>
2018-06-06 18:01:21 -0700

    Setting up constants

---
+++ Kasha/Kasha/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Kasha
+//
+//  Created by Elliott Kipper on 6/6/18.
+//  Copyright © 2018 Kip. All rights reserved.
+//
+
+import UIKit
+
+class Constants {
+    static let musicID = "music.com.kip.kasha"
+    static let musicKeyID = "AVTL547TZF"
+    static let teamID = "LSB86U552U"
+}
