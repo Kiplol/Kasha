a89863f38252d4ced153e45bda9b18604ae0c495
Elliott "Kip" Kipper <Kip@Phyllis.local>
2018-06-10 09:55:06 -0700

    Sections

--- Kasha/Kasha/Artists/ArtistsViewController.swift
+++ Kasha/Kasha/Artists/ArtistsViewController.swift
@@ -10,7 +10,7 @@ import AsyncDisplayKit
 import MediaPlayer
 import UIKit
 
-class ArtistsViewController: ASViewController<ASDisplayNode>, ASTableDataSource, ASTableDelegate {
+class ArtistsViewController: KashaViewController, ASTableDataSource, ASTableDelegate, UITableViewDelegate {
 
     // MARK: - ivars
     fileprivate let tableNode = ASTableNode()
@@ -35,14 +35,36 @@ class ArtistsViewController: ASViewController<ASDisplayNode>, ASTableDataSource,
     
     // MARK: - Helpers
     func artist(forRowAt indexPath: IndexPath) -> MPMediaItemCollection {
-        guard let artist = self.allArtistsQuery.collections?[indexPath.row] else {
+        guard let section = self.artists(forSection: indexPath.section) else {
+            preconditionFailure("No artist section for row at indexPath \(indexPath)")
+        }
+        let currentLocation = section.range.location + indexPath.row
+        guard let artist = self.allArtistsQuery.collections?[currentLocation] else {
             preconditionFailure("No artist for row at indexPath \(indexPath)")
         }
         return artist
     }
+    
+    func artists(forSection section: Int) -> MPMediaQuerySection? {
+        return self.allArtistsQuery.collectionSections?[section]
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionIndexTitles(for: tableView)?[section]
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+//        return ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+//                "V", "W", "X", "Y", "Z", "#"]
+        return self.allArtistsQuery.collectionSections?.compactMap { $0.title }
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return index
+    }
 
     // MARK: - ASTableDataSource
-    
     func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
         let artist = self.artist(forRowAt: indexPath)
         
@@ -56,11 +78,11 @@ class ArtistsViewController: ASViewController<ASDisplayNode>, ASTableDataSource,
     
     // MARK: - ASTableDelegate
     func numberOfSections(in tableNode: ASTableNode) -> Int {
-        return 1
+        return self.allArtistsQuery.collectionSections?.count ?? 0
     }
     
     func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
-        return self.allArtistsQuery.collections?.count ?? 0
+        return self.artists(forSection: section)?.range.length ?? 0
     }
 
 }
--- Kasha/Kasha/View Controllers/KashaViewController.swift
+++ Kasha/Kasha/View Controllers/KashaViewController.swift
@@ -6,30 +6,13 @@
 //  Copyright © 2018 Kip. All rights reserved.
 //
 
+import AsyncDisplayKit
 import UIKit
 
-class KashaViewController: UIViewController {
+class KashaViewController: ASViewController<ASDisplayNode> {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
