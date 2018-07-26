b18588118918dc54b2e7aa9787dbf8b4430f0889
Elliott "Kip" Kipper <>
2018-07-26 11:16:46 -0700

    Handle unauthorized

--- Kasha/Kasha/Onboarding/SplashViewController.swift
+++ Kasha/Kasha/Onboarding/SplashViewController.swift
@@ -11,6 +11,23 @@ import UIKit
 
 class SplashViewController: KashaViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var labelExplanation: UILabel!
+    @IBOutlet weak var buttonSettings: UIButton! {
+        didSet {
+            self.buttonSettings.applyAlbumsStyle()
+        }
+    }
+    @IBOutlet var unauthorizedViews: [UIView]!
+    
+    // MARK: - KashaViewController
+    override func apply(theme: Theme) {
+        super.apply(theme: theme)
+        self.labelExplanation.textColor = theme.textColor
+        self.buttonSettings.backgroundColor = theme.primaryAccentColor
+        self.buttonSettings.setTitleColor((theme.primaryAccentColor.isDark ? .white: .black), for: .normal)
+    }
+    
     // MARK: - View Lifecycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -20,10 +37,24 @@ class SplashViewController: KashaViewController {
             case .authorized:
                 DispatchQueue.main.async {
                     AppDelegate.instance.enterApp()
+                    self.unauthorizedViews.forEach { $0.isHidden = true }
                 }
             default:
-                break
+                DispatchQueue.main.async {
+                    self.unauthorizedViews.forEach { $0.isHidden = false }
+                }
             }
+        }
+    }
+    
+    // MARK: - User Interaction
+    @IBAction func settingsTapped(_ sender: Any) {
+        guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: nil)
         }
     }
     
